15e1c940dafb28f9751db641c46fde94029952b3
Corwin Moyne <c_moyne@yahoo.com>
2020-10-19 22:30:01 +0100

    feat: add data from newsapi.org

--- Home.js
+++ Home.js
@@ -1,10 +1,54 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { useEffect } from "react/cjs/react.development";
 
-export default function HomePage() {
+export default function HomePage({ navigation }) {
+  const [dataLoading, finishLoading] = useState(true);
+  const [newsData, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://newsapi.org/v2/everything?q=tech&apiKey=d5bb4ed674b34e8f8512924bd197e50e"
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json.articles))
+      .catch((error) => console.error(error))
+      .finally(() => finishLoading(false));
+  }, []);
+
+  const storyItem = ({ item }) => {
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => navigation.navigate("NewsDetail", { url: item.url })}
+      >
+        <View style={styles.listings}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Image style={styles.thumbnail} source={{ uri: item.urlToImage }} />
+          <Text style={styles.blurb}>{item.description}</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>This will be the home pg</Text>
+      {dataLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={newsData}
+          renderItem={storyItem}
+          keyExtractor={(item) => item.url}
+        />
+      )}
     </View>
   );
 }
@@ -15,5 +59,24 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    width: "100%",
+    padding: 20,
+  },
+  thumbnail: {
+    height: 100,
+    width: "98%",
+  },
+  listings: {
+    paddingBottom: 15,
+    paddingTop: 15,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+  title: {
+    paddingBottom: 10,
+    fontWeight: "bold",
+  },
+  blurb: {
+    fontStyle: "italic",
   },
 });
